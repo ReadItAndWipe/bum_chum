06ac63c573ea9f2387f233da40b5e0b51e25bf93
Oscar <oscar.kennedysmith@gmail.com>
2020-05-25 20:27:56 +1000

    npm initialised

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 // Sets port if deploying to external provider 
 // or port assigned already
