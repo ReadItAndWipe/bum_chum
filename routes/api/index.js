a0199c17e762354dcb8a3b54a5776338c1e0c82d
Shauna Mack <shaunakatemack@gmail.com>
2020-05-28 15:10:11 +1000

    add subscription model and view

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-//Import Roll schema
+//Import Roll and Subsc schema
 const Roll = require('../../models/Roll');
+const Subscription = require('../../models/Subscription');
 
 // @route    GET api/
 // @desc     Landing page shows all rolls  
@@ -10,8 +11,9 @@ const Roll = require('../../models/Roll');
 router.get('/', async (req, res) => {
   //Retrieve all rolls from database 
   try {
+    let subscriptions = await Subscription.find()
     let rolls = await Roll.find()
-    res.render('home', { rolls } )
+    res.render('home', { rolls, subscriptions } )
   } catch (error) {
     if(!rolls){
       return res.status(501).json("out of rolls")
